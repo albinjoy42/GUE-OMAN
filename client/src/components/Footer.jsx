914067bbf20641adb8428c78a7da385e4f88a8fe
Albin Joy <albinjoy42@gmail.com>
2023-10-13 12:27:18 +0530

    COmpleted

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer()
+{
+  return (
+    <>
+        <div class="footer">
+            <p>
+                Copyright @ 2023 by Albin & Tony. All Rights Reserved.
+                GUE Oman is Powered by Camerinfolks  Pvt Ltd.
+            </p>
+        </div>
+    </>
+  )
+}
+
+export default Footer;
